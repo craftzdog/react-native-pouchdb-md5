b23c8b3c2ccf3b359e4436818dc9af27d60094dc
Takuya Matsuyama <hi@craftz.dog>
2021-02-17 15:38:07 +0900

    fix(native): calculating md5 from binary string is not working

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,14 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { atob, btoa } from 'js-base64';
+import { Buffer } from 'buffer';
+
+// @ts-ignore
+global.atob = atob;
+// @ts-ignore
+global.btoa = btoa;
+// @ts-ignore
+global.Buffer = Buffer;
 
 AppRegistry.registerComponent(appName, () => App);
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
-import Md5 from 'react-native-quick-md5';
+import {
+  binaryMd5 as nativeBinaryMd5,
+  stringMd5 as nativeStringMd5,
+  // @ts-ignore
+} from 'react-native-quick-md5';
 // @ts-ignore
 import NativeFileReaderModule from 'react-native/Libraries/Blob/NativeFileReaderModule.js';
+
+const g = global as any;
 
 type CallbackFunction = (base64Hash: string) => any;
 
 export function binaryMd5(data: string | Blob, callback: CallbackFunction) {
   if (typeof data === 'string') {
-    const md5 = Md5.calc(data, 'utf8', 'base64');
+    const md5Hex = nativeBinaryMd5(data);
+    const md5 = g.Buffer.from(md5Hex, 'hex').toString('base64');
     setImmediate(() => callback(md5));
   } else {
     // @ts-ignore
     NativeFileReaderModule.readAsDataURL(data.data).then((url: string) => {
       const base64Data = url.split(',')[1];
-      const md5 = Md5.calc(base64Data, 'base64', 'base64');
+      const binStr = g.atob(base64Data);
+      const md5Hex = nativeBinaryMd5(binStr);
+      const md5 = g.Buffer.from(md5Hex, 'hex').toString('base64');
       callback(md5);
     });
   }
 }
 export function stringMd5(data: string) {
-  return Md5.calc(data, 'utf8', 'hex');
+  return nativeStringMd5(data);
 }
 
 export default {
